7f79c329fe246afe263b05962661c77e1322f2ae
Vando Ramos <vandorramos@gmail.com>
2023-04-22 15:22:43 -0300

    configuring database, posts controller and routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show ]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end  
+
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+    else
+      render :new
+    end    
+  end
+  
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:description)
+  end
+end
