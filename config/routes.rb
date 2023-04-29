e28fad65971aad1f238b24b593c8f85c0ed0dc03
Vando Ramos <vandorramos@gmail.com>
2023-04-29 16:01:08 -0300

    authentication page with confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: %i[ sessions registrations passwords ]
+  
   root 'posts#index'
 
   resources :posts, only: %i[ index show new create ]
