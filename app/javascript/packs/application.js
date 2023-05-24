64ae3e2ac0fe0b05d24872d493f78e07c18776ba
Vando Ramos <vandorramos@gmail.com>
2023-05-24 10:11:38 -0300

    Synchronizing posts with Action Cable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import Rails from "@rails/ujs";
 import * as ActiveStorage from "@rails/activestorage";
-// import "channels"
+import "channels"
 
 import "../src/application.css";
 
